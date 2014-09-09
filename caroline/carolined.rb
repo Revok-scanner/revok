ca0b7d0f6a5c71f4b8895e72cea88709d47b10d6
yuzheng <yuzheng@redhat.com>
2014-09-09 14:24:22 +0800

    Update promotes, bug fix for rest cleanup and rm useless code in caroline
    Change-Id: I3425b674afb45b21ffe2ff7ea24ec1bfaeea2ec0

--- caroline/carolined.rb
+++ caroline/carolined.rb
@@ -6,15 +6,6 @@ require 'model.Server.RunCase'
 $CAROLINE_PATH="#{$ROOT_PATH}/caroline"
 active = true
 
-def update
-  fresh=true
-  Dir.chdir($ROOT_PATH) do
-    system '/usr/bin/git submodule init 2>/dev/null'
-    fresh = system '([ `/usr/bin/git submodule update 2>/dev/null | wc -l` -le 1 ] && [ `/usr/bin/git pull 2>/dev/null| wc -l` -le 1 ])'     
-  end
-  return (not fresh)
-end
-
 def clean_exit
   puts "Goodbye, Caroline!"
   if $runCaseServer
@@ -26,19 +17,6 @@ end
 
 $runCaseServer=RunCaseServer.new
 
-trap("SIGINT") do
-  if active 
-    puts "\nSir! We're still doing science. Give me a few seconds to clean up."
-    active = false
-  else 
-    if $runCaseServer
-      $runCaseServer.clean 
-      $runCaseServer=nil
-    end
-    raise SystemExit
-  end
-end
-
 to_run = []
 begin
   while active do
@@ -49,9 +27,6 @@ begin
       puts "Checking for test scans to be run..."
       runCase=$runCaseServer.getRunCaseFromQueue
       if runCase
-        if runCase.scanConfigObj.screenshot==0
-          clean_exit if update
-        end
         $runCaseServer.ackRunCaseToQueue
         result=$runCaseServer.validate(runCase)
         to_run << runCase if result
