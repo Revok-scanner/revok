684cb9c765aaa90a5da7e83e25dadbabc1c4ae79
yuzheng <yuzheng@redhat.com>
2014-09-24 10:06:14 +0800

    bugfix: remove fragment from url for access_admin
    Change-Id: Ifc9805a318ab70723b7b150f74e85f87d960268b

--- revok/modules/access_admin.rb
+++ revok/modules/access_admin.rb
@@ -32,7 +32,7 @@ class AdminAccessor
 
     data = JSON.parse(@session_data, {create_additions:false})
     config = JSON.parse(@config, {create_additions:false})
-    cookie=data['cookie'].gsub(/security=high/,"security=low")
+    cookie=data['cookie']
 
     logtype = config['logtype']
     username = config['username']
@@ -49,7 +49,7 @@ class AdminAccessor
       flg = false
     end
 
-    target = target + '/' if !flg
+    target = target.split("#")[0]
 
     log "Sending requests to possible admin URIs..." 
 
@@ -71,7 +71,8 @@ class AdminAccessor
           g_uri,
           method: :get,
           headers: { Cookie: cookie, Authorization: auth},
-          connecttimeout:5
+          connecttimeout:5,
+          ssl_verifypeer:false
         )
       else
         req= Typhoeus::Request.new(
@@ -79,12 +80,13 @@ class AdminAccessor
           method: :get,
           headers: {Cookie: cookie},
           connecttimeout:5,
+          ssl_verifypeer:false
         )
       end
 
       begin
         resp = req.run
-        if resp.code == 200 or (resp.code > 300 and resp.code < 308 and resp.response_headers.grep(/login/) == [])
+        if resp.code == 200 or (resp.code > 300 and resp.code < 308 and resp.response_headers.scan(/login/) == [])
           if resp.code > 300 and resp.code < 308 and resp.response_body.scan(/<input .*?value="(Login|login|log in|Log in)"/)
             next
           end
