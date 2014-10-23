f901c49b8649468f5ff06125c8d2729069de3947
Connor Yan <126ium@gmail.com>
2014-10-23 18:44:39 +0800

    Add missing requirement files
    Signed-off-by: Connor Yan <126ium@gmail.com>

--- revok/revok.rb
+++ revok/revok.rb
@@ -1,6 +1,10 @@
 $LOAD_PATH << File.dirname(__FILE__)
 
+require_relative 'core/config'
+require_relative 'core/modules'
+require_relative 'core/module'
 require_relative 'core/framework'
+require_relative 'core/module_executor'
 
 module Revok
 
