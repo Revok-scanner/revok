aa562733c1aef5521e4a8ccbf196a6dadad8149f
chinazoujy <chinazoujy@gmail.com>
2014-11-18 11:03:53 +0800

    Modified access_admin to fill new framework

--- caroline/modules/access_admin.rb
+++ caroline/modules/access_admin.rb
@@ -1,59 +1,60 @@
 #
 # Access Admin Pages Module
 # Send HTTP requests with common administrative URIs to check whether these pages can be accessed by the provides user account.
-# 
-
-$: << "#{File.dirname(__FILE__)}/lib/"
-
-require 'report.ut'
+#
 require 'json'
-require 'admin_url'
 require 'rex'
 require 'typhoeus'
+require 'core/module'
+require "#{Revok::Config::MODULES_DIR}/lib/report.ut.rb"
+require "#{Revok::Config::MODULES_DIR}/lib/admin_url.rb"
 
-class AdminAccessor
+class AdminAccessor < Revok::Module
   include AdminURLs
   include ReportUtils
 
-  def initialize(config=$datastore['config'],session_data=$datastore['session'],flag='s')
-    @config=config
-    if flag=='f'
-      @session_data=File.open(session_data).read
+  def initialize(load_from_file = false, session_file="", flag='s')
+    info_register("Access_admin_pages", {"group_name" => "default",
+                              "group_priority" => 10,
+                              "detail" => "Send HTTP requests with common administrative URIs to check whether these pages can be accessed by the provides user account."
+                              "priority" => 10})
+    if(load_from_file)
+        @session_data = File.open(session_file).read
     else
-      @session_data=session_data
+        @session_data = ""
     end
   end
-  
-  def run
 
+  def run
+    @session_data = @datastore['session'] if @session_data == nil
+    config = @datastore['config']
     def encode64(msg)
       Rex::Text::encode_base64(msg)
     end
-
-    data = JSON.parse(@session_data, {create_additions:false})
-    config = JSON.parse(@config, {create_additions:false})
+    begin
+      data = JSON.parse(@session_data, {create_additions:false})
+      config = JSON.parse(config, {create_additions:false})
+    rescue => exp
+      Log.debug(exp.to_s)
+      Log.debug(exp.backtrace.join("\n"))
+      return
+    end
     cookie=data['cookie']
-
     logtype = config['logtype']
     username = config['username']
     password = config['password']
     target = config['target']
-
     encoded_auth = encode64(username + ":" + password)
-
     urls = AURLS
     rpt_uri = Array.new()
     flg = true
-
     if (target.scan(/\/$/) == [])
       flg = false
     end
-
     target = target.split("#")[0]
-
-    log "Sending requests to possible admin URIs..." 
-
+    Log.info( "Sending requests to possible admin URIs..." )
     urls.each do |a_uri|
+      
       #generate a GET request for each url in the list
       req = Hash.new()
 
@@ -94,7 +95,7 @@ class AdminAccessor
            rpt_uri.push uri
         end
       rescue
-        log "ERROR: #{$!}" 
+        Log.error("#{$!}")
         break
       end
     end
@@ -107,8 +108,6 @@ class AdminAccessor
     else
       abstain
     end
-    log "access_admin is done"
-
+    Log.info("Access admin pages is done")
   end
-
 end
