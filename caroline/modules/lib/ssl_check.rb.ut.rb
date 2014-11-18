89a1efdf153c161152d640ce6f350759ca2c22b5
chinazoujy <chinazoujy@gmail.com>
2014-11-18 12:45:54 +0800

    Update ssl_check.rb to fill the new framework

--- caroline/modules/lib/ssl_check.rb.ut.rb
+++ caroline/modules/lib/ssl_check.rb.ut.rb
@@ -2,141 +2,141 @@ require 'rexml/document'
 require 'time'
 module SSLCheck
 
-    def init(url,cert_file)
-      @uri=URI(url)
-      @host=@uri.host
-      @port=@uri.port
-      @end_flag=false;
-      @ssl_report=Hash.new()
-      @cipherList=[]
+    def init(url, cert_file)
+      @uri = URI(url)
+      @host = @uri.host
+      @port = @uri.port
+      @end_flag = false;
+      @ssl_report = Hash.new()
+      @cipherList = []
 
-      get_ssl_socket(@host,@port,cert_file)
-      @ssl_scan_file=`mktemp`.strip
-      log "Run sslscan..." 
+      get_ssl_socket(@host, @port, cert_file)
+      @ssl_scan_file = `mktemp`.strip
+      Log.info "Run sslscan..."
       `sslscan --no-failed --xml=#{@ssl_scan_file} #{@host}:#{@port}`
-      log "sslscan end" 
-      xmldata= File.read("#{@ssl_scan_file}")
-      xmldata=xmldata.gsub(/&/, '&amp;')
+      Log.info "sslscan end"
+      xmldata = File.read("#{@ssl_scan_file}")
+      xmldata = xmldata.gsub(/&/, '&amp;')
 
       @doc = REXML::Document.new(xmldata)
-      if @doc==nil
-        log "REXML load sslscan xmlfile fail" 
-        @end_flag=ture
+      if @doc == nil
+        Log.warn "REXML load sslscan xmlfile fail"
+        @end_flag = ture
       else
         if @doc.root != nil
           @doc.root.each_element('//ssltest/cipher') do |item|
             @cipherList.push(item)
           end
         else
-          log "scanssl failed" 
-          @end_flag=true;
+          Log.warn "scanssl failed"
+          @end_flag = true;
         end
       end
     end
 
-    def get_ssl_socket(host,port,trusted_cert_file)
+    def get_ssl_socket(host, port, trusted_cert_file)
       tcp_client = TCPSocket.new(host, port)
       ssl_context = OpenSSL::SSL::SSLContext.new
       ssl_context.verify_mode = OpenSSL::SSL::VERIFY_PEER
       ssl_context.ca_file = trusted_cert_file
-      log "Connect with CA certificate file: #{ssl_context.ca_file}" 
+      Log.info "Connect with CA certificate file: #{ssl_context.ca_file}"
 
       begin
         ssl_client = OpenSSL::SSL::SSLSocket.new(tcp_client, ssl_context)
-        ssl_client= ssl_client.connect
+        ssl_client = ssl_client.connect
       rescue => exp
         if exp.class.name == "OpenSSL::SSL::SSLError"
-          reason="The site's security certificate is not trusted."
-          advice="Use a trusted certificate for your site."
-          @ssl_report[:issuer_check]=[reason,advice]
+          reason = "The site's security certificate is not trusted."
+          advice = "Use a trusted certificate for your site."
+          @ssl_report[:issuer_check] = [reason,advice]
         end
       end
-      tcp_client.close if tcp_client!=nil
-      ssl_client.close if ssl_client!=nil
+      tcp_client.close if tcp_client != nil
+      ssl_client.close if ssl_client != nil
     end
 
-    def run_check 
-      @end_flag||sslv2_check
-      @end_flag||nullCipher_check
-      @end_flag||weakCipher_check
-      @end_flag||md5SignedCert_check
-      @end_flag||date_check
-      @end_flag||ssl_forcing_check
+    def run_check
+      @end_flag || sslv2_check
+      @end_flag || nullCipher_check
+      @end_flag || weakCipher_check
+      @end_flag || md5SignedCert_check
+      @end_flag || date_check
+      @end_flag || ssl_forcing_check
       return @ssl_report
     end
 	
     def sslv2_check
-      log "sslv2 check" 
-      if @cipherList.any?{ |item| item.attribute('sslversion')=='SSLv2'}
-        reason="SSLv2 protocol is supported."
-        advice="Use SSLv3 or TLSv1 protocol."
-        @ssl_report[:sslv2_check]=[reason,advice]
+      Log.info "sslv2 check"
+      if @cipherList.any?{ |item| item.attribute('sslversion') == 'SSLv2'}
+        reason = "SSLv2 protocol is supported."
+        advice = "Use SSLv3 or TLSv1 protocol."
+        @ssl_report[:sslv2_check] = [reason,advice]
       end
     end
 
     def nullCipher_check
-      log "nullCipher check" 
-      if @cipherList.any?{ |item| item.attributes['bits']=='0'}
-        reason="Null cipher is supported."
-        advice="Use strong ciphers."
-        attach=@cipherList.find_all{|x| x.attributes['bits']=='0'}.map{|y| "#{y.attributes['sslversion']} #{y.attributes['bits']}bits #{y.attributes['cipher']}"}
-        @ssl_report[:nullCipher_check]=[reason,advice,attach]
+      Log.info "nullCipher check"
+      if @cipherList.any?{ |item| item.attributes['bits'] == '0'}
+        reason = "Null cipher is supported."
+        advice = "Use strong ciphers."
+        attach = @cipherList.find_all{|x| x.attributes['bits'] == '0'}.map{|y| "#{y.attributes['sslversion']} #{y.attributes['bits']}bits #{y.attributes['cipher']}"}
+        @ssl_report[:nullCipher_check] = [reason, advice, attach]
       end
     end
 
     def weakCipher_check
-      log "weakCipher check" 
-      if @cipherList.any?{ |item| item.attributes['bits'].to_i < 112 and item.attributes['bits'].to_i>0}
-        reason="Weak ciphers(key length <112bits) are found."
-        advice="Use strong ciphers."
-        attach=@cipherList.find_all{|x| x.attributes['bits'].to_i < 112 and x.attributes['bits'].to_i>0 }.map{|y| "#{y.attributes['sslversion']} #{y.attributes['bits']}bits #{y.attributes['cipher']}"}
-        @ssl_report[:weakCipher_check]=[reason,advice,attach]
+      Log.info "weakCipher check"
+      if @cipherList.any?{ |item| item.attributes['bits'].to_i < 112 and item.attributes['bits'].to_i > 0}
+        reason = "Weak ciphers(key length < 112bits) are found."
+        advice = "Use strong ciphers."
+        attach = @cipherList.find_all{|x| x.attributes['bits'].to_i < 112 and x.attributes['bits'].to_i > 0 }.map{|y| "#{y.attributes['sslversion']} #{y.attributes['bits']}bits #{y.attributes['cipher']}"}
+        @ssl_report[:weakCipher_check] = [reason, advice, attach]
       end
     end
 
     def md5SignedCert_check
-      log "md5SignedCert check" 
-      @doc.root.each_element('//ssltest/certificate/signature-algorithm') do |item| 
-        if item.get_text.value=~/[Mm][Dd]5/
-          reason="Md5 signature algorithm is used."
-          advice="Use advanced signature algorithm for the certificate."
-          @ssl_report[:md5SignedCert_check]=[reason,advice]
+      Log.info "md5SignedCert check"
+      @doc.root.each_element('//ssltest/certificate/signature-algorithm') do |item|
+        if item.get_text.value =~ /[Mm][Dd]5/
+          reason = "Md5 signature algorithm is used."
+          advice = "Use advanced signature algorithm for the certificate."
+          @ssl_report[:md5SignedCert_check] = [reason, advice]
         end
       end
     end
 
     def date_check
-      status=0
-      reason=""
-      attache=nil
-      log "date check" 
-      @doc.root.each_element('//ssltest/certificate/not-valid-before') do |item| 
+      status = 0
+      reason = ""
+      attache = nil
+      Log.info "date check"
+      @doc.root.each_element('//ssltest/certificate/not-valid-before') do |item|
         if Time.parse(item.get_text.value) > Time.now
           reason = "The certificate is invalid."
-          status=1
-          attache=["Not valid before: #{item.get_text.value} (Now: #{Time.now})"]
+          status = 1
+          attache = ["Not valid before: #{item.get_text.value} (Now: #{Time.now})"]
         end
       end
       @doc.root.each_element('//ssltest/certificate/not-valid-after') do |item| 
         if Time.parse(item.get_text.value) < Time.now
           reason = "The certificate is out of date."
-          status=1
-          attache=["Not valid after: #{item.get_text.value} (Now: #{Time.now})"]
+          status = 1
+          attache = ["Not valid after: #{item.get_text.value} (Now: #{Time.now})"]
         end
       end
       if status == 1
-        advice="Set appropriate term of validity for the certificate."
-        @ssl_report[:date_check]=[reason,advice,attache]
+        advice = "Set appropriate term of validity for the certificate."
+        @ssl_report[:date_check] = [reason, advice, attache]
       end
     end
  
     def ssl_forcing_check
-      log "Strict-Transport-Security check" 
-      resp=`(echo -e \"GET #{@uri} HTTP/1.1\nHost: #{@host}\n\n\";sleep 10)|openssl s_client -connect #{@host}:#{@port} 2>/dev/null`
-      if not resp.upcase.include?"STRICT-TRANSPORT-SECURITY"
-        reason="Strict-Transport-Security is not set."
-        advice="Add Strict-Transport-Security header."
-        @ssl_report[:ssl_forcing_check]=[reason,advice]
+      Log.info "Strict-Transport-Security check"
+      resp = `(echo -e \"GET #{@uri} HTTP/1.1\nHost: #{@host}\n\n\";sleep 10)|openssl s_client -connect #{@host}:#{@port} 2>/dev/null`
+      if not resp.upcase.include?("STRICT-TRANSPORT-SECURITY")
+        reason = "Strict-Transport-Security is not set."
+        advice = "Add Strict-Transport-Security header."
+        @ssl_report[:ssl_forcing_check] = [reason, advice]
       end
     end
 
--- caroline/modules/ssl_check.rb
+++ caroline/modules/ssl_check.rb
@@ -3,51 +3,53 @@
 # Check SSL/TLS mis-configuration which makes the communication between browser and server not secure, such as weak cipher, invalid certificate etc.
 #
 
-$: << "#{File.dirname(__FILE__)}/lib/"
-require 'report.ut'
 require 'timeout'
 require 'net/http'
 require 'typhoeus'
-require 'ssl_check.rb.ut'
-class SSLChecker
+require 'core/module'
+require "#{Revok::Config::MODULES_DIR}/lib/ssl_check.rb.ut"
+
+class SSLChecker < Revok::Module
   include SSLCheck
-  include ReportUtils
-  def initialize(config=$datastore['config'],cert_file="#{File.dirname(__FILE__)}/ca-bundle.trust.crt")
-    @config=config
-    @cert_file=cert_file
+
+  def initialize
+    info_register("SSL_checker", {"group_name" => "default",
+                                "group_priority" => 10,
+                                "priority" => 10,
+                                "detail" => "Check SSL/TLS mis-configuration which makes the communication between browser and server not secure, such as weak cipher, invalid certificate etc."})
   end
 
   def https_check(url)
-    uri=URI(url)
-    sch=uri.scheme
-    if sch=='https'
+    uri = URI(url)
+    sch = uri.scheme
+    if sch == 'https'
       return true
-    elsif sch=='http'
+    elsif sch == 'http'
       return false
     end
   end
 
   def ssl_available_check
-    time=0
+    time = 0
     if not https_check(@url)
       while time < 5
         if @url.end_with?("/")
-          slash=true
+          slash = true
         else
-          slash=false
+          slash = false
         end
-        response =  Typhoeus::Request.new(@url,ssl_verifypeer: false,ssl_verifyhost: 1,connecttimeout:5,).run
-        if response!= nil && ((response.code>= 300 && response.code<=303) || response.code==307)
+        response =  Typhoeus::Request.new(@url, ssl_verifypeer: false, ssl_verifyhost: 1, connecttimeout:5,).run
+        if response != nil && ((response.code >= 300 && response.code <= 303) || response.code == 307)
           response.response_headers.match(/^\s*Location\:\s*(.*?)$/i)
           if $1.start_with?('http')
-            @url=$1
+            @url = $1
           else
             if $1.start_with?('/') && slash
-                @url=@url.slice(0..-2)+$1
+                @url = @url.slice(0..-2) + $1
             elsif not ($1.start_with?('/') or slash)
-              @url=@url+'/'+$1
+              @url = @url + '/' + $1
             else
-              @url=@url+$1
+              @url = @url + $1
             end
           end
           if https_check(@url.delete!("\r"))
@@ -56,48 +58,51 @@ class SSLChecker
         else
           return false
         end
-        time=time+1
+        time = time + 1
       end
     end
 
     begin
-      response =  Typhoeus::Request.new(@url,ssl_verifypeer: false,ssl_verifyhost: 1,connecttimeout:5,).run
-      if response != nil && response.code!=404 && response.code!=0
+      response =  Typhoeus::Request.new(@url, ssl_verifypeer: false, ssl_verifyhost: 1, connecttimeout:5,).run
+      if response != nil && response.code != 404 && response.code != 0
         return true
       end
     rescue => excep
-      log "ERROR: #{excep}"
+      error
+      Log.error(exp.to_s)
+      Log.debug(exp.backtrace.join("\n"))
     end
     return false
   end
 
   def run
-    abstain
-    config = JSON.parse(@config, {create_additions:false})
-    @url=config['target'].delete("\C-M")
-    uri=URI(@url)
-    log "Checking SSL/TLS mis-configuration..."
-
     begin
+      Log.info("Checking SSL/TLS mis-configuration...")
+      @config = @datastore['config']
+      config = JSON.parse(@config, {create_additions:false})
+      @url = config['target'].delete("\C-M")
+      uri = URI(@url)
       if ssl_available_check == true
-        @url=@url.gsub("http://", "https://")
+        @cert_file = "#{Revok::Config::MODULES_DIR}/ca-bundle.trust.crt"
+        @url = @url.gsub("http://", "https://")
         init(@url,@cert_file)
-        ssl_report=run_check
+        ssl_report = run_check
       else
-        ssl_report={ssl_available_check:["Target URL does not appear to support SSL.","Enable SSL for your site, or at least make sure the urls which transmit sensitive data are only accessible through secure HTTPS connections."]}
+        ssl_report = {ssl_available_check:["Target URL does not appear to support SSL.","Enable SSL for your site, or at least make sure the urls which transmit sensitive data are only accessible through secure HTTPS connections."]}
       end
-    rescue => excep
+    rescue => exp
       error
-      log "ERROR: #{excep}"
+      Log.error(exp.to_s)
+      Log.debug(exp.backtrace.join("\n"))
     ensure
       finalize
     end
 
-    if ssl_report!=nil && ssl_report.length>0
+    if ssl_report! = nil && ssl_report.length > 0
       advise({"ssl_report" => ssl_report})
+    else
+      abstain
     end
-
-    #log "#{ssl_report}"
-    log "ssl_check is done"
+    Log.info("ssl_check is done")
   end
 end
