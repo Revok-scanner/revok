2cb6ba6a8e2f14e74ac9da837e2de7bddcd75251
chinazoujy <chinazoujy@gmail.com>
2014-11-18 12:38:35 +0800

    Update path_traversal.rb

--- revok/modules/path_traversal.rb
+++ revok/modules/path_traversal.rb
@@ -2,57 +2,45 @@
 # Path Traversal Checking Module
 # Check whether content of a file can be read by injecting its file path to requests.
 #
-
-$: << "#{File.dirname(__FILE__)}/lib/"
-require 'report.ut'
 require 'json'
-require 'path_traversal.rb.ut'
+require 'core/module'
+require "#{Revok::Config::MODULES_DIR}/lib/report.ut.rb"
+require "#{Revok::Config::MODULES_DIR}/lib/path_traversal.rb.ut.rb"
 
-class PathTravelor
+class PathTravelor < Revok::Module
   include PATH_TRAV
   include ReportUtils
-  def initialize(config=$datastore['config'],session_data=$datastore['session'],flag='s')
-    @config=config
-    if flag=='f'
-      begin
-        @session_data=File.open(session_data,'r').read 
-      rescue =>exp
-        log exp.to_s 
-        @session_data=""
-      end
-    elsif flag=="s"
-      @session_data=session_data
-    else
-      log 'unknow flag' 
-      return nil
-    end
+  def initialize(load_from_file = false, session_file = "")
+    info_register("PathTravelor", {"group_name" => "default",
+                              "group_priority" => 10,
+                              "priority" => 10})
   end
 
   def run
     vul_url = Array.new()
-
     #Filter the URLs that have a parameter which is a file
-    log "Filtering URLs with file parameters..."
+    Log.info( "Filtering URLs with file parameters...")
+    begin
     tcks, params = filter_url_request
-
+    rescue => exp
+       Log.error("#{exp}")
+       return
+    end
     if tcks != {}
       #Perform path traversal against the URLs filtered above
       vul_url = path_trav(tcks, params)
     end
-
     if vul_url == "error"
-       error
+      Log.error("#{exp}")
     elsif vul_url != []
       vul_url.each do |v|
         m = v[0]
         v = v[1].gsub(/\\/, "")
         list(v, 'mthd' => m)
       end
-      warn
+     
     else
-      abstain
+#      abstain
     end
-    log "path_traversal is done"
-
   end
 end
