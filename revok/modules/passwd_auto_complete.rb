692325da0c09bfe72444c24782ed4780d3d9ffb5
chinazoujy <chinazoujy@gmail.com>
2014-11-18 12:37:50 +0800

    Update passwd_auto_complete.rb

--- revok/modules/passwd_auto_complete.rb
+++ revok/modules/passwd_auto_complete.rb
@@ -2,23 +2,24 @@
 # Auto Complete Password Module
 # Check whether password field turns off the autocomplete option.
 #
-
-$: << "#{File.dirname(__FILE__)}/lib/"
-require 'report.ut'
 require 'nokogiri'
 require 'open-uri'
 require 'json'
 require 'openssl'
+require 'core/module'
+require "#{Revok::Config::MODULES_DIR}/lib/report.ut.rb"
 
-class PasswordAutoCompleteChecker
+class PasswordAutoCompleteChecker < Revok::Module
   include ReportUtils
-  def initialize(config=$datastore['config'])
-    @config=config
+  def initialize
+    info_register("PasswordAutoCompleteChecker", {"group_name" => "default", 
+                                "group_priority" => 10,
+                                "priority" => 10})
   end
-
+  
   def find_auto_complete(node)
     if node.to_s.include?("autocomplete=\"off\"")
-      log "auto-complete is not enabled"
+      Log.warn("auto-complete is not enabled")
       return true
     elsif node.name == "form" || node.name == "html"
       return false
@@ -26,28 +27,29 @@ class PasswordAutoCompleteChecker
       return find_auto_complete(node.parent)
     end
   end
-
+  
   def run
-    log "Checking auto-complete attribute of password parameters..." 
+    Log.info("Checking auto-complete attribute of password parameters...") 
     result = false
     issues = []
     target = ""
     begin
+      @config = @datastore['config']
       config = JSON.parse(@config, {create_additions:false})
       if config['logtype'] == "normal"
         target = config['login']
-        log "Access login page #{target}" 
+        Log.info("Access login page #{target}" )
         doc = Nokogiri::HTML(open(target, :ssl_verify_mode => OpenSSL::SSL::VERIFY_NONE))
         passwd_element = doc.css("input[type='password']")
 
         if !passwd_element.empty?
           result = find_auto_complete(passwd_element[0])
         else
-          log "Invalid login page." 
+          Log.error("Invalid login page.") 
           result = true
         end
       else
-        log "Login-type is not form-based" 
+        Log.warn( "Login-type is not form-based" )
         result = true
       end
     rescue => exp 
@@ -59,14 +61,13 @@ class PasswordAutoCompleteChecker
     else
       if issues.size != 0
         issues.each do |issue|
-          log "ERROR: #{issue}\n" 
+          Log.info("#{issue}\n")
         end
         error
         return
       end
       advise({'login_page'=>target})
-      log "auto-complete is enabled"
+      Log.info( "auto-complete is enabled")
     end
-
   end
 end
