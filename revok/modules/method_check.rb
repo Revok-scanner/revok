1e45fdbdfdff5752b42ef2efab55d2c62e5faa5e
chinazoujy <chinazoujy@gmail.com>
2014-11-18 12:35:14 +0800

    Update method_check.rb

--- revok/modules/method_check.rb
+++ revok/modules/method_check.rb
@@ -2,30 +2,27 @@
 # HTTP Method Checking Module
 # Check whether some HTTP methods such as 'TRACE' and 'OPTIONS' for URLS are available.
 #
-
-$: << "#{File.dirname(__FILE__)}/lib/"
-require 'report.ut'
 require 'net/http'
 require 'json'
+require 'core/module'
+require "#{Revok::Config::MODULES_DIR}/lib/report.ut.rb"
 
-class MethodCheckor
+class MethodCheckor < Revok::Module
   include ReportUtils
-  def initialize(session_data=$datastore['session'],flag='s')
-    if flag=='f'
+  def initialize(load_from_file = false, session_file = "")
+    info_register("MethodCheckor", {"group_name" => "default",
+                              "group_priority" => 10,
+                              "priority" => 10})
+    if(load_from_file)
       begin
-        @session_data=File.open(session_data,'r').read 
-      rescue =>exp
-        log "ERROR: #{exp.to_s}" 
-        @session_data=""
+        @session_data = File.open(session_file, 'r').read
+      rescue => exp
+        @session_data = ""
+        Log.warn(exp.to_s)
+        Log.debug("#{exp.backtrace}")
       end
-    elsif flag=="s"
-      @session_data=session_data
-    else
-      log 'unknow flag' 
-      return nil
     end
   end
-
 
   def method_check(uri)
     methods = Array.new()
@@ -52,7 +49,6 @@ class MethodCheckor
     if (response.code == "200")
       methods.push("TRACE")
     end
-
     return methods
   end
 
@@ -62,13 +58,17 @@ class MethodCheckor
     result = true
     vul_paths = Hash.new()
     begin
+      @session_data = @datastore['session'] if @session_data == nil
       data = JSON.parse(@session_data, {create_additions:false})
       @sitemap=data['sitemap']
       urls_list = @sitemap
-      raise ArgumentError, "Lack of the sitemap" if urls_list == nil
+      if urls_list == nil then
+         Log.error("Lack of the sitemap" )
+         return 
+      end 
 
       #checking http method
-      log "Checking http methods for each directory..." 
+      Log.info( "Checking http methods for each directory..." )
       urls_list.each {|url|
         uri = URI(url)
         if !uri.path.include?(".")
@@ -87,26 +87,24 @@ class MethodCheckor
     end
 
     if result
-      abstain
+      Log.warn("The result is none")
     else
       if issues.size > 0
         issues.each do |issue|
-          log "ERROR: #{issue}" 
+          Log.error( "#{issue}" )
         end
         error
         return
       end
       if !vul_paths.empty?
         vul_paths.each_pair {|path, method|
-          log "The URL \"#{path}\" should disable following methods:" 
-          log method.to_s 
+          Log.info( "The URL \"#{path}\" should disable following methods:" )
+          Log.warn( method.to_s )
           list(path, {'method'=>"#{method}"})
         }
       end
       advise({"vul_paths" => vul_paths})
     end
-    log "method_check is done"
-
   end
 
 end
