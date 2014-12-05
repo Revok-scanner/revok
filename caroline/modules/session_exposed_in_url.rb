3f2097fb521ae499e0c64973c069f37aeb5d6da5
Connor Yan <126ium@gmail.com>
2014-12-06 03:06:38 +0800

    Update session_exposed_in_url.rb to fill the new framework
    Signed-off-by: Connor Yan <126ium@gmail.com>

--- caroline/modules/session_exposed_in_url.rb
+++ caroline/modules/session_exposed_in_url.rb
@@ -3,31 +3,29 @@
 # Check whether session id is exposed in requested URLs.
 #
 
-$: << "#{File.dirname(__FILE__)}/lib/"
-require 'report.ut'
 require 'json'
 require 'typhoeus'
 require 'net/http'
 require 'rex'
-require 'session_check.rb.ut'
+require 'core/module'
+require "#{Revok::Config::MODULES_DIR}/lib/session_check.rb.ut.rb"
 
-include Sess
-class SessionExposureCheckor 
-  include ReportUtils
-  def initialize(config=$datastore['config'],session_data=$datastore['session'],flag='s')
-    @config=config
-    if flag=='f'
+class SessionExposureChecker < Revok::Module
+  include Sess
+
+  def initialize(load_from_file = false, session_file = "")
+    info_register("Session_ID_Exposure_Checker", {"group_name" => "default",
+                              "group_priority" => 10,
+                              "priority" => 10,
+                              "detail" => "Check whether session id is exposed in requested URLs."})
+    if (load_from_file)
       begin
-        @session_data=File.open(session_data,'r').read 
-      rescue =>exp
-        log exp.to_s 
-        @session_data=""
+        @session_data = File.open(session_file, 'r').read
+      rescue => exp
+        @session_data = ""
+        Log.warn(exp.to_s)
+        Log.debug(exp.backtrace.join("\n"))
       end
-    elsif flag=="s"
-      @session_data=session_data
-    else
-      log 'unknow flag' 
-      return nil
     end
   end
   
@@ -49,45 +47,46 @@ class SessionExposureCheckor
     login_page = login.split("/")[login.split("/").size-1]
 
     if logtype == "normal"
-      #Scan for login request    
+      #Scan for login request
       requests.each_pair do |k,v|
         body = v.gsub(v.split("\r\n\r\n")[0], "")
-        if v.scan(/^POST/)!=[] and body!= "" and body.scan(/#{username}/)!=[] and body.scan(/#{password}/)!=[]
+        if v.scan(/^POST/) != [] and body != "" and body.scan(/#{username}/) != [] and body.scan(/#{password}/) != []
           login_request = k.to_i
           @login_request = login_request
-          $datastore['login_request'] = @login_request
+          @datastore['login_request'] = @login_request
           break
         end
       end
  
-      #Scan for the first request after login 
+      #Scan for the first request after login
       if login_request > 0 and requests["#{login_request+1}"] != nil
-        req  = requests["#{login_request+1}"]
+        req = requests["#{login_request+1}"]
       else
-        log "Login request or request after login isn't scanned." 
+        Log.info "Login request or request after login isn't scanned."
         return
       end
     end
 
     #Detect session ID from cookies
     cookies.each do |k|
-      k=k.split("=")[0]
+      k = k.split("=")[0]
       if logtype == "normal"
         test_req = req.gsub(/#{k}=.*?;|#{k}=.*?$/, "")
-        url=req.match(/\s(http.*?)\sHTTP\//)[1]
-        uri=URI(url)
-        host=uri.host
+        url = req.match(/\s(http.*?)\sHTTP\//)[1]
+        uri = URI(url)
+        host = uri.host
         port = uri.port
         context = {}
-        ssl = (uri.scheme=='https'?true:false)
+        ssl = (uri.scheme == 'https' ? true : false)
         ssl_version = nil
         proxies = nil
-        conn=Rex::Proto::Http::Client.new(host,port, context, ssl, ssl_version, proxies)
+        conn = Rex::Proto::Http::Client.new(host, port, context, ssl, ssl_version, proxies)
 
         begin
           resp = conn.send_recv(test_req, 30)
-        rescue
-          log "ERROR: #{$!}" 
+        rescue => exp
+          Log.error(exp.to_s)
+          Log.debug(exp.backtrace.join("\n"))
         end
 
         if resp.headers['Location'] != nil
@@ -107,19 +106,26 @@ class SessionExposureCheckor
     end
 
     if session_id != ""
-      @session_id=session_id
-      $datastore['session_id'] = session_id
-      log "The key of session ID is detected: #{session_id}" 
+      @session_id = session_id
+      @datastore['session_id'] = session_id
+      Log.info "The key of session ID is detected: #{session_id}"
     else
-      log "Session ID isn't detected" 
+      Log.info "Session ID isn't detected"
     end
 
   end
   
-  def exposure_check
+  def run
     session_val = String.new
     url = String.new
     vul = Array.new
+    @config = @datastore['config']
+    @session_data = @datastore['session'] if @session_data == nil
+
+    Log.info "Detecting session id..."
+    session_id_detect
+
+    Log.info "Checking session id exposure in urls..."
     begin
       if @session_id != nil
         session = JSON.parse(@session_data, {create_additions:false})
@@ -138,24 +144,24 @@ class SessionExposureCheckor
           end
         end
       end
-    rescue => excep
+    rescue => exp
       error
-      log excep.to_s 
+      Log.error(exp.to_s)
+      Log.debug(exp.backtrace.join("\n"))
     end
 
     if vul.size == 0
       abstain
-      log "No session ID found in urls"  
     else
-      vul=vul.uniq
+      vul = vul.uniq
       vul.each do |k|
-        log "Session exposed in url of request: #{k}" 
+        Log.warn "Session exposed in url of request: #{k}"
         k = k.gsub(/POST/, "POST request for").gsub(/GET/, "GET request for")
-        list("#{k}")  
+        list("#{k}")
       end
       warn({"name" => "session_exposed_in_url"})
     end
-
+    Log.info("Session id exposure check completed")
   end
   
 end
